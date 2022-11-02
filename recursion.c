b3c038b99c264dee7ac3482579e6d265d9d5dde1
slin14 <61814005+slin14@users.noreply.github.com>
2022-11-01 21:01:12 -0700

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,181 @@
+/*
+ File:          recursion.c
+ Purpose:       Exercises for CPSC259 lab 4
+ Author:			  Your names
+ Student #s:		70196886
+ CWLs:		      slin14
+ Date:				  Nov. 1, 2022
+ */
+
+ /******************************************************************
+	PLEASE EDIT THIS FILE
+
+	Comments that start with // should be replaced with code
+	Comments that are surrounded by * are hints
+	******************************************************************/
+
+
+	/* Preprocessor directives */
+#include "recursion.h"
+//#include <string.h>
+
+/*
+ Calculates the power.
+ PARAM:     base is an integer
+ PARAM:     power is an integer
+ PRE:       power >= 0
+ PRE:       base != 0
+ RETURN:    base^power
+ */
+int calculate_power(int base, int power)
+{
+	// Replace this return statement with your own code
+	if (power == 0) {
+		return 1;
+	}
+	else {
+		return base * calculate_power(base, power - 1);
+	}
+	return -1;
+}
+
+/*
+ Returns the number of digits in an integer
+ PARAM:     number is an integer
+ PRE:       0 < number <= INT_MAX
+ RETURN:    the number of digits in the number
+ */
+int count_digits(int number)
+{
+	// Replace this return statement with your own code
+	if ((number / 10) == 0) {
+		return 1;
+	}
+	else {
+		return 1 + count_digits(number / 10);
+	}
+}
+
+/*
+ Returns the length of the specified string.
+ PARAM:  string, a pointer to an array of char
+ PRE:    the string pointer is not a dangling pointer
+ RETURN: the length of the string passed as a parameter
+ */
+int string_length(char* string)
+{
+	// Replace this return statement with your own code
+	if (string[0] == '\0') {
+		return 0;
+	}
+	else {
+		return 1 + string_length(string + 1);
+	}
+}
+
+/*
+ Determines if a string is a palindrome.  DOES NOT skip spaces!
+ For example,
+ ""     -> returns 1 (an empty string is a palindrome)
+ "the"  -> returns 0
+ "abba" -> returns 1
+ "Abba" -> returns 0
+ "never odd or even" -> returns 0
+ For the recursive call, be aware that both parameters can be modified.
+ PARAM:  string, a pointer to an array of char
+ PARAM:  string_length, the length of the string
+ PRE:    the string pointer is not a dangling pointer
+ PRE:    string_length is the correct length of the string
+ RETURN: IF string is a palindrome
+				 THEN 1
+		 END 0
+ */
+int is_palindrome(char* string, int string_length)
+{
+	// Replace this return statement with your own code
+	if (string_length <= 1) {
+		return 1;
+	}
+	if (string_length == 2) {
+		if (string[0] == string[string_length-1]) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (string[0] == string[string_length-1]) {
+		return is_palindrome(string + 1, string_length - 2);
+	}
+	else {
+		return 0;
+	}
+}
+
+/*
+ Draws a ramp.  The length of the longest rows is specified by the parameter.
+ For the recursive call, be aware that both parameters can be modified.
+ PARAM:     number, an integer
+ PARAM:     buffer, a character array of sufficient length
+ PRE:       number >= 1
+						buffer has sufficient length and contains null characters from the buffer
+						address to the end of its length
+ POST:      draws a ramp whose height is the specified number into buffer
+ RETURN:    the number of characters written into buffer. Don't forget to count your
+						newline characters!
+ */
+int draw_ramp(int number, char* buffer)
+{
+	int ret_val = 0;
+	if (number == 0) {
+		return 0;
+	}
+	if (number == 1) {
+		ret_val = draw_row(number, buffer);
+		//strcat(buffer, "\n");
+		//return ret_val;
+	}
+	else {
+		ret_val = draw_row(number, buffer) + 1;
+		strcat(buffer, "\n");
+		ret_val = ret_val + draw_ramp(number - 1, buffer);
+		strcat(buffer, "\n"); 
+		ret_val = ret_val + draw_row(number, buffer) + 1;
+		//strcat(buffer, "\n");
+		//return ret_val;
+		if (ret_val > strlen(buffer)) {
+			buffer[ret_val] = '\0';
+		}
+	}
+	return ret_val;
+}
+
+/*
+ Draws a row of asterisks of the specified length
+ For the recursive call, be aware that both parameters can be modified.
+ PARAM:  size, an integer
+ PARAM:  buffer, a character array of sufficient length
+ PRE:    size >= 1
+				 buffer has sufficient length and contains null characters from the buffer
+				 address to the end of its length
+ POST:   draws a row of asterisks of specified length to buffer
+ RETURN: the number of characters drawn
+ */
+int draw_row(int size, char* buffer)
+{
+	if (size == 0) {
+		return 0;
+	}
+	else if (size == 1) {
+		strcat(buffer, "*");
+		//printf("draw_row 1, size = %d, buffer = %s\n", size, buffer);
+		//printf("draw_row 1, size = %d, buffer_len = %d\n", size, strlen(buffer));
+		return 1;
+	}
+	else {
+		strcat(buffer, "*");
+		//printf("draw_row *, size = %d, buffer = %s\n", size, buffer);
+		//printf("draw_row *, size = %d, buffer_len = %d\n", size, strlen(buffer));
+		return 1 + draw_row(size - 1, buffer);
+	}
+}
